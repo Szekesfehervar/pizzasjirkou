e7a167e5ae9da684b59ad1bac056620426fc278d
Šimůnek Josef <u17.428@pslib.cloud>
2021-01-14 13:22:06 +0100

    hotovo2

--- src/pages/calzone.js
+++ src/pages/calzone.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React,{useContext, useState} from 'react';
+import {IngredientsContext} from "../components/ingredientsProvider";
+import IngredientsDisplayPizza from "../components/ingredientsDisplayPizza";
+import IngredientsDisplay from "../components/ingredientsDisplay";
 
 const Calzone = () => {
+  const [ingredients, setIngredients] = useContext(IngredientsContext);
+  const [pizzaIng, setPizzaIng] = useState([]);
+  const AddIng = (props) => {
+    if(pizzaIng.length <4){
+    setPizzaIng(pizzaIng => [...pizzaIng,{name:props.name, category:props.category}]);}
+  }
+  
     return(
         <div>
         <h1>Calzone</h1>
+        {ingredients.map(m =>(<IngredientsDisplayPizza name={m.name} category={m.category}  AddIng={AddIng}></IngredientsDisplayPizza>))}   
+          <br/>    
+          <h1>Your ingredients</h1>
+          {pizzaIng.map(m =>(<IngredientsDisplay name={m.name} category={m.category}></IngredientsDisplay>))}
       </div>
 
 );
